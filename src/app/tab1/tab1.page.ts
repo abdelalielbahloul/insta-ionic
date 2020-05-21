524fa9a48c3af8898a66f36964a0b552fd14d00b
Abdelali EL BAHLOUL <bahloul97abdel@gmail.com>
2020-05-21 21:02:30 +0000

    feat : add actions buttons and post description under posts

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -22,7 +22,7 @@ export class Tab1Page {
     },
     {
       imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRxxk25rXO6kSdibEKMQ7niBmZZxrK5xIEzV8AjUgHKzy_ZEQG1&usqp=CAU', 
-      name: 'hind.louzi'
+      name: 'Hind.louzi'
     },
     {
       imageURL: 'https://benjaminjurke.com/assets/images/authors/benjamin-jurke-v6.jpg', 
@@ -46,6 +46,31 @@ export class Tab1Page {
     
     }
 
+  ]
+
+  posts = [
+    {
+      userAvatar: 'https://pbs.twimg.com/media/DZotU1hW0AEDN5F.jpg',
+      userName: 'maxim alex',
+      postImage: 'https://media.nesta.org.uk/images/Predictions-2019_Twitter_02.width-1200.png',
+      likes: '2.875',
+      description: 'Abusus enim multitudine hominum, quam tranquillis in rebus diutius rexit, ex agrestibus habitaculis urbes construxit multis opibus firmas et viribus, quarum ad praesens pleraeque licet'
+    },
+    {
+      userAvatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRxxk25rXO6kSdibEKMQ7niBmZZxrK5xIEzV8AjUgHKzy_ZEQG1&usqp=CAU',
+      userName: 'Hind.louzi',
+      postImage: 'https://3.bp.blogspot.com/-FUVfdLVzt08/Uv5j8ZBp0vI/AAAAAAAAFmE/yneznNlDgIY/s1600/Bacteria-bacilicus-subtilis.png',
+      likes: '875',
+      description: 'Abusus enim multitudine hominum, quam tranquillis in rebus diutius rexit, ex agrestibus habitaculis urbes construxit multis opibus firmas et viribus, quarum ad praesens pleraeque licet'
+    },
+    {
+      userAvatar: 'https://benjaminjurke.com/assets/images/authors/benjamin-jurke-v6.jpg',
+      userName: 'felexkamello',
+      postImage: 'https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+      likes: '275',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati consectetur labore reprehenderit quos quis cum aut aperiam magni, rem distinctio. Beatae voluptatibus nam ab sint necessitatibus eius quia nostrum porro!'
+    }
+    
   ]
 
 
